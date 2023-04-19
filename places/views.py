6a902ac307ccfae84fd8a231c1273c49fc900bad
S1im_Sh4dy <mihakurd@gmail.com>
2023-04-19 17:17:17 +0300

    - Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,64 @@
+import json
 from django.shortcuts import render
+from .models import Place
 
-# Create your views here.
+
+def show(request):
+    places = Place.objects.prefetch_related('images')
+
+    geojson_positions = {
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [37.62, 55.793676]
+                },
+                'properties': {
+                    'title': 'Легенды Москвы',
+                    'placeId': 'moscow_legends',
+                    'detailsUrl': "static/places/moscow_legends.json",
+                }
+            },
+            {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [37.64, 55.753676]
+                },
+                'properties': {
+                    'title': 'Крыши24.рф',
+                    'placeId': 'roofs24',
+                    'detailsUrl': "/static/places/roofs24.json",
+                }
+            }
+        ],
+    }
+    # for place in places:
+    #     geo_place = {
+    #         'title': place.title,
+    #         'imgs': [obj.image.url for obj in place.images.all()],
+    #         'description_short': place.description_short,
+    #         'description_long': place.description_long,
+    #         'placeId': 'moscow_legends',
+    #         'coordinates': {
+    #             'lng': place.lng,
+    #             'lat': place.lat,
+    #         },
+    #     }
+    #
+    #     geojson_positions['features'].append({
+    #         'type': 'Feature',
+    #         'geometry': {
+    #             'type': 'Point',
+    #             'coordinates': [place.lng, place.lat],
+    #         },
+    #         'properties': {
+    #             'title': place.title,
+    #             'placeId': geo_place["placeId"],
+    #             'detailsUrl': f'/static/places/{geo_place["placeId"]}.json',
+    #         },
+    #     })
+
+    return render(request, 'index.html', {'places': geojson_positions})
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'places.apps.PlacesConfig',
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'where_to_go.urls'
@@ -114,6 +116,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -131,3 +136,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type("application/javascript", ".js", True)
