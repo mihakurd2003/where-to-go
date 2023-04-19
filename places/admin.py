1c3ce6bc2272a9f9cfb7a4c92a82110086ba8b09
S1im_Sh4dy <mihakurd@gmail.com>
2023-04-19 20:59:40 +0300

    - Added photo preview

--- places/admin.py
+++ places/admin.py
@@ -1,17 +1,29 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['photo']
+    fields = ['image', 'photo', 'position']
+
+    def photo(self, obj):
+        return format_html(
+            f'<img src="{obj.image.url}" width="{obj.image.width // 4}" height="{obj.image.height // 4}"/>'
+        )
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ['title']
     inlines = [ImageInline]
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    ...
+    readonly_fields = ['photo']
+
+    def photo(self, obj):
+        return format_html(
+            f'<img src="{obj.image.url}" width="{obj.image.width // 3}" height="{obj.image.height // 3}"'
+        )
